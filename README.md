d7fdd908a0150184a1fa3f6b94af8930d511a7f5
Ann Markham <ann.markham@outlook.com>
2020-12-12 11:20:56 -0700

    update readme and clean up comments

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-Link to Heroku app https://annm-notetaker.herokuapp.com/
+# Project Title: Note-Taker
+
+![License](https://img.shields.io/badge/License-mit-blue.svg)
+
+## Description An application for writing and saving notes.
+
+## Table of Contents:
+
+1. [Installation] (#installation)
+1. [Usage](#usage)
+1. [Credits](#credits)
+1. [License](#license)
+1. [Tests](#tests)
+1. [Questions](#questions)
+
+## Installation Clone Repo. Install node.js and npm express, & uuidv4. Deploy to location that supports node.js and express.
+
+## Usage Note-Taker is deployed here: https://annm-notetaker.herokuapp.com/
+
+Screenshots: ![](public/assets/images/screenshot of deployed pages showing notes entered, delete buttons.PNG)
+Screenshots: ![](public/assets/images/Screenshot of landing page deployed on Heroku.PNG)
+
+### click on "Get Started" Button
+
+### Type in Note Title and Note Text, then click the save icon to save the note.
+
+### To open saved notes, click on the Note Title on the left side of the page. The selected note will appear on the right.
+
+### To delted saved note, click on the trash can icon.
+
+### Clicking on "Note Taker" in the nav bar will take you back to the "Get Started" landing page.
+
+## License mit
+
+### License Link "https://choosealicense.com/licenses/mit",
+
+## Questions
+
+For more information about this or other projects check out my GitHub page or send me an email.
+
+### GitHub Page AnnMarkham
+
+### Email Address ann.markham@outlook.com
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,52 +1,43 @@
-// const { db } = require('./db/db');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const {readNotes, writeNotes, findById} = require('../lib/notes.js');  
 
-// const notes = require('../lib/notes.js');
-//II.  API Routes 
-  // import readNotes function from lib/notes.js
- const {readNotes, writeNotes, findById} = require('../lib/notes.js');  
-
-// II -1. GET/api/notes (When open page)
-//a. read db.json (Create function (readNotes function in lib/notes.js); call in get function below).  &
+//  GET/ Notes  (When open page)
+//a. read db.json &  b.  return all saved notes as json 
  router.get('/notes', (req, res) => {
-  notes = readNotes(); // a.  call function created in notes.js
-//b.  return all saved notes as json (update get function to res.join(result of readNotes function call))
+  notes = readNotes(); 
   res.json(notes); 
 });
 
-// get a saved note by id
+// GET a Saved note by id
 router.get('/notes/:id', (req, res) => {
-  notes = readNotes();      // call readNotes function created in notes.js & save value in notes variable
+  notes = readNotes();     
   const result = findById(req.params.id, notes);  //(call findById function created in notes.js -- filters notesArray for note.id)
   res.json(result);
 }); 
 
-// II -2.  POST/api/notes 
-  //a. receive a new note to
+// POST Notes
+  //receives a new note 
   router.post('/notes', (req, res) => {  
-  //b. save on the request body,       
+  //save note on the request body,       
   const newNote = req.body;                 
-  //give each note a unique id when it is saved (look into npm packages that could do this for you) 
-  newNote.id= uuidv4(); 
-  //c. add it (new note) to the db.json file, and then  d. return the new note to the client
+   newNote.id= uuidv4(); //give note unique id
+  //add new note to the db.json file, and then  d. return the new note to the client
   notes.push(newNote);
   writeNotes(notes);
   res.json(newNote);
 });
 
-//BONUS -  Handle Delete Requests
-// III - DELETE/api/notes: id
-//a. receives a query parameter containing the id of the note to delete. 
+// BONUS - DELETE Notes
+//receives a query parameter containing the id of the note to delete. 
 router.delete('/notes:id?', (req, res) => { 
  const id = req.query.id;
-  notes = readNotes();
- //ii. remove the note with the given id property
+ notes = readNotes();
+ //remove the note with the given id property
   newNotes = notes.filter(note => note.id != id);
-  //iii. rewrite the notes to the db.json file
+  //rewrite the notes to the db.json file
   writeNotes(newNotes);
   res.send(JSON.stringify(newNotes));
 });
-
 
 module.exports = router;
